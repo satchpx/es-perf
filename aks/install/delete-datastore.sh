776494c7315a113c638548513c8cf55a319da3d4
Sathya Balakrishnan <sathya@portworx.com>
2019-07-31 14:16:42 -0700

    update helper scripts
    Signed-off-by: Sathya Balakrishnan <sathya@portworx.com>

--- aks/install/delete-datastore.sh
+++ aks/install/delete-datastore.sh
@@ -6,5 +6,5 @@ helm delete --purge datastore-elasticsearch-master
 # Delete client
 helm delete --purge datastore-elasticsearch-client
 # Delete pvc
-kubectl get pvc -l app=datastore-elasticsearch-master -o name | xargs kubectl delete -f
-kubectl get pvc -l app=datastore-elasticsearch-client -o name | xargs kubectl delete -f
+kubectl get pvc -l app=datastore-elasticsearch-master -o name | xargs kubectl delete
+kubectl get pvc -l app=datastore-elasticsearch-client -o name | xargs kubectl delete
