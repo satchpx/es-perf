1e10716e5e0fc0d66fae908b7e0b457ff35b90d6
Sathya Balakrishnan <sathya@portworx.com>
2019-08-12 15:03:42 -0700

    update install scripts
    Signed-off-by: Sathya Balakrishnan <sathya@portworx.com>

--- install/install-gateway.sh
+++ install/install-gateway.sh
@@ -1,5 +1,51 @@
 #!/usr/bin/env bash
 
-### Deploy gateway (kafka)
-helm  upgrade --wait --timeout=600 --install --values manifests/kafka-values-gp2.yaml gateway helm-charts/confluent
+printUsage() {
+  cat <<EOUSAGE
+Usage:
+  $0 
+    -s <storage provider> [px|mp|gp2]
+    -r <replication factor> [1|2]
+EOUSAGE
+  echo "Example: install-datastore.sh px"
+}
 
+while getopts "h?:s:r:" opt; do
+    case "$opt" in
+    h|\?)
+        printUsage
+        exit 0
+        ;;
+    s)  STORAGE_PROVIDER=$OPTARG
+        ;;
+    r)  RF=$OPTARG
+        ;;
+    :)
+        echo "[ERROR] Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+    default)
+       printUsage
+       exit 1
+    esac
+done
+
+### Deploy gateway (kafka)
+if [ ${STORAGE_PROVIDER} == "gp2" ]; then
+    helm  upgrade --wait --timeout=600 --install --values manifests/kafka-values-gp2.yaml gateway helm-charts/confluent
+elif [${STORAGE_PROVIDER} == "mp" ]; then
+    helm  upgrade --wait --timeout=600 --install --values manifests/kafka-values-mp.yaml gateway helm-charts/confluent
+elif [ ${STORAGE_PROVIDER} == "px" ]; then
+    if [ ${RF} -eq 1 ]; then
+        helm  upgrade --wait --timeout=600 --install --values manifests/kafka-values-px-rf1.yaml gateway helm-charts/confluent
+    elif [ ${RF} -eq 2 ]; then
+        helm  upgrade --wait --timeout=600 --install --values manifests/kafka-values-px-rf2.yaml gateway helm-charts/confluent
+    else
+        echo "[ERROR]: Unknown replication factor ${RF}"
+        exit 1
+    fi
+else
+    echo "[ERROR]: Don't know what you're upto"
+    printUsage
+    exit 1
+fi
