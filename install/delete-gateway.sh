1c4370ca56bda3f07f6dee45d42f0d85ee3064e6
Sathya Balakrishnan <sathya@portworx.com>
2019-08-12 14:53:52 -0700

    update install scripts
    Signed-off-by: Sathya Balakrishnan <sathya@portworx.com>

--- install/delete-gateway.sh
+++ install/delete-gateway.sh
@@ -3,3 +3,5 @@
 ### Delete gateway (kafka)
 helm  delete --purge gateway
 
+kubectl get pvc -l app=cp-kafka -o name | xargs kubectl delete
+kubectl get pvc -l app=cp-zookeeper -o name | xargs kubectl delete
